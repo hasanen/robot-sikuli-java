79189d978dbeac12912cca5c8711494ff725b20f
Joni Hasanen <joni.hasanen@eficode.com>
2014-04-08 10:54:04 +0300

    Added xpresser

--- cookbooks/common/recipes/default.rb
+++ cookbooks/common/recipes/default.rb
@@ -1,7 +1,10 @@
+execute "add-apt-repository 'deb http://ppa.launchpad.net/xpresser-team/ppa/ubuntu precise main'" do
+  action :run
+end
 execute 'apt-get update' do
   action :run
 end
-execute 'apt-get upgrade' do
+execute 'apt-get upgrade -y' do
   action :run
 end
 package 'xubuntu-desktop'
@@ -11,3 +14,7 @@ package 'libopencv-core2.4'
 package 'libopencv-highgui2.4'
 package 'libopencv-imgproc2.4'
 package 'openjdk-7-jdk'
+package 'python-xpresser' do
+  action :install
+  options "--force-yes"
+end
